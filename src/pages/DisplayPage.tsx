b58d3ff8b7e0e97e57bb53082990031b76321c0f
benjaexz <jacopimentelxd@gmail.com>
2026-05-26 16:38:27 -0400

    feat: add fullscreen display mode

--- src/pages/DisplayPage.tsx
+++ src/pages/DisplayPage.tsx
@@ -1,40 +1,94 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { Header } from '../components/Header'
 import { Timer } from '../components/Timer'
 import { formatTime } from '../utils/formatTime'
 import { INITIAL_TIMER_SECONDS } from '../constants/timer'
 import { listenTimerState } from '../services/timerService'
-import { useRef } from 'react'
 
 export function DisplayPage() {
-  const [seconds, setSeconds] = useState(INITIAL_TIMER_SECONDS)
+  const [seconds, setSeconds] = useState(
+    INITIAL_TIMER_SECONDS,
+  )
+
+  const [isFullscreen, setIsFullscreen] =
+    useState(false)
+
+  const pageRef =
+    useRef<HTMLElement | null>(null)
 
   useEffect(() => {
-    const unsubscribe = listenTimerState((timerState) => {
-      if (!timerState) return
+    const unsubscribe =
+      listenTimerState((timerState) => {
+        if (!timerState) return
 
-      setSeconds(timerState.seconds)
-    })
+        setSeconds(
+          timerState.seconds,
+        )
+      })
 
     return () => unsubscribe()
   }, [])
 
-  const formattedTime = formatTime(seconds)
+  useEffect(() => {
+    function handleFullscreenChange() {
+      setIsFullscreen(
+        Boolean(
+          document.fullscreenElement,
+        ),
+      )
+    }
 
- return (
-  <main className="display-app">
-    <section className="timer-card display-mode">
-      <Header />
+    document.addEventListener(
+      'fullscreenchange',
+      handleFullscreenChange,
+    )
 
-      <p className="session-label">
-        Sessão Plenária
-      </p>
+    return () => {
+      document.removeEventListener(
+        'fullscreenchange',
+        handleFullscreenChange,
+      )
+    }
+  }, [])
 
-      <Timer
-        time={formattedTime}
-        seconds={seconds}
-      />
-    </section>
-  </main>
-)
+  function enterFullscreen() {
+    pageRef.current?.requestFullscreen()
+  }
+
+  const formattedTime =
+    formatTime(seconds)
+
+  return (
+    <main
+      ref={pageRef}
+      className="display-app"
+    >
+      <section
+        className="
+          timer-card
+          display-mode
+        "
+      >
+        <Header />
+
+        {!isFullscreen && (
+          <button
+            className="fullscreen-button"
+            onClick={enterFullscreen}
+          >
+            Entrar em Tela Cheia
+          </button>
+        )}
+
+        <p className="session-label">
+          Sessão Plenária
+        </p>
+
+        <Timer
+          time={formattedTime}
+          seconds={seconds}
+        />
+      </section>
+    </main>
+  )
 }
